6b0b6212d3c3ed06a1bb321ee26b089c83f7940c
hdelcarpiom <140546274+hdelcarpiom@users.noreply.github.com>
2024-03-29 14:01:56 -0500

    terminado

--- Capitulo 1/Algoritmo_Fizz_Buzz2.py
+++ Capitulo 1/Algoritmo_Fizz_Buzz2.py
@@ -1,12 +1,9 @@
-listado = input ("Ingrese una lista de numeros : ")
-listado = list(listado)
-print(type(listado))
-for numero in listado:
-    if int(numero) % 3 and int(numero) % 5:
+for numero in range(1,100):
+    if int(numero) % 3 == 0 and int(numero) % 5 == 0:
         print("FIZZBUZZ,")
     elif int(numero) % 3 ==0:
         print("FIZZ,")
     elif int(numero) % 5 == 0:
         print("Buzz,")
     else:
-        print(numero + ",")
+        print(str(numero) + ",")
