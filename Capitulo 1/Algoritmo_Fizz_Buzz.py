8c85cd95a10f936d39b3dcadb4568464a2d3b6bd
hdelcarpiom <140546274+hdelcarpiom@users.noreply.github.com>
2024-03-29 13:27:06 -0500

    FIZZBUZZ 1

---
+++ Capitulo 1/Algoritmo_Fizz_Buzz.py
@@ -0,0 +1,10 @@
+listado = input ("Ingrese una lista de numeros : ")
+listado = list(listado)
+print(type(listado))
+for numero in listado:
+    if int(numero) % 3 ==0:
+        print("FIZZ,")
+    elif int(numero) % 5 == 0:
+        print("Buzz,")
+    else:
+        print(numero + ",")
